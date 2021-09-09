c02a3fb344e3691b83313567e038548736941368
aregularguy <bagwanarshad180@gmail.com>
2021-09-09 23:35:32 +0530

    gitignore change

--- app.js
+++ app.js
@@ -64,8 +64,11 @@ const PORT= process.env.PORT || 3000
 
 
 app.listen(
-    PORT,
-     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-     
-     )
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
+
+    //  app.listen(process.env.PORT || 3000, function(){
+    //   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+    // });
 
